037a287f6c42fdfd7992b42af0c783062615df8a
kimdg1105 <kimss7334@naver.com>
2021-08-09 09:39:38 +0900

    21.08.09 - 사용자 정의 레포지토리 (Querydsl)

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+import study.querydsl.repository.custom.MemberRepositoryCustom;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    List<Member> findByUsername(String username);
+
+
+}
